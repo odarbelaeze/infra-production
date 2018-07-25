56107cf3b3d7662dbec490f0756cf402f7ba584c
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-07-25 00:19:17 -0500

    Too sleepy

--- ci/server-update.sh
+++ ci/server-update.sh
@@ -1,3 +1,3 @@
 scp -o "StrictHostKeyChecking no" -i ci/keys/id_rsa -r balancer/ quotes/ root@$SERVER_IP:
-ssh -o "StrictHostKeyChecking no" -i ci/keys/id_rsa root@$SERVER_IP: 'docker stack deploy -c balancer/docker-compose.yml balancer'
-ssh -o "StrictHostKeyChecking no" -i ci/keys/id_rsa root@$SERVER_IP: 'docker stack deploy -c quotes/docker-compose.yml quotes'
+ssh -o "StrictHostKeyChecking no" -i ci/keys/id_rsa root@$SERVER_IP 'docker stack deploy -c balancer/docker-compose.yml balancer'
+ssh -o "StrictHostKeyChecking no" -i ci/keys/id_rsa root@$SERVER_IP 'docker stack deploy -c quotes/docker-compose.yml quotes'
